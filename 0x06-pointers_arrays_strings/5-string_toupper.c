202b629ca468622d92eb3d7b7ae8f8426ca5816f
vangats <vangats19@gmail.com>
2023-03-29 13:52:27 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes lowercase to uppercase
+ * @n: input string
+ * Return: capitialized string
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
